d0ada806ae84fb69bc9758ee4529b3cc70ad7103
architJoshi2904 <aj6082@rit.edu>
2023-06-24 15:43:09 +0530

    Java-Mario Clone : Entity and Tile class skeleton

---
+++ src/com/main/mario/tile/Tile.java
@@ -0,0 +1,56 @@
+package com.main.mario.tile;
+
+import java.awt.*;
+
+public class Tile {
+
+    public int x, y;    // Position classes
+    public int width, height;
+    public boolean solid;
+
+    public int velX;
+    public int velY;
+
+    public Tile(int x, int y, int width, int height, boolean solid) {
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+        this.solid = solid;
+    }
+
+    public void render( Graphics g ) {
+        // Uses graphics object+buffer strategy from Game class
+
+    }
+
+    public void tick() {
+        x += velX;
+        y += velY;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setX( int x ) {
+        this.x = x;
+    }
+
+    public void setY( int y ) {
+        this.y = y;
+    }
+
+
+    public void setVelX( int velX ) {
+        this.velX = velX;
+    }
+
+    public void setVelY( int velY ) {
+        this.velY = velY;
+    }
+}
