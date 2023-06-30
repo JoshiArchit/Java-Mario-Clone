5d5398f00775b84b697beb3514437bf9ccc0d361
architJoshi2904 <aj6082@rit.edu>
2023-06-30 21:20:34 +0530

    Java-Mario Clone : Added Rectangle methods for collision detection

--- src/com/main/mario/entity/Entity.java
+++ src/com/main/mario/entity/Entity.java
@@ -1,8 +1,11 @@
 package com.main.mario.entity;
 
+import com.main.mario.Handler;
+import com.main.mario.Id;
+
 import java.awt.*;
 
-public class Entity {
+public abstract class Entity {
 
     public int x, y;    // Position classes
     public int width, height;
@@ -11,24 +14,23 @@ public class Entity {
     public int velX;
     public int velY;
 
-    public Entity(int x, int y, int width, int height, boolean solid) {
+    public Id id;
+
+    public Handler handler;
+
+    public Entity(int x, int y, int width, int height, boolean solid, Id id, Handler handler) {
         this.x = x;
         this.y = y;
         this.width = width;
         this.height = height;
         this.solid = solid;
+        this.id = id;
+        this.handler = handler;
     }
 
-    public void render( Graphics g ) {
-        // Uses graphics object+buffer strategy from Game class
+    public abstract void render( Graphics g );
 
-    }
-
-    public void tick() {
-        x += velX;
-        y += velY;
-    }
-
+    public abstract void tick();
     public int getX() {
         return x;
     }
@@ -52,5 +54,36 @@ public class Entity {
 
     public void setVelY( int velY ) {
         this.velY = velY;
+    }
+
+    public boolean isSolid(){
+        return solid;
+    }
+    public Id getId() {
+        return id;
+    }
+
+    public void die() {
+        handler.removeEntity( this );
+    }
+
+    public Rectangle getBounds() {
+        return new Rectangle(getX(), getY(), width, height);
+    }
+
+    public Rectangle getBoundsTop() {
+        return new Rectangle(getX()+10, getY(), width-20, 5);
+    }
+
+    public Rectangle getBoundsBottom() {
+        return new Rectangle(getX()+10, getY()+height-5, width-20, 5);
+    }
+
+    public Rectangle getBoundsLeft() {
+        return new Rectangle(getX(), getY()+10, 5, height-20);
+    }
+
+    public Rectangle getBoundsRight() {
+        return new Rectangle(getX()+width-5, getY()+10, 5, height-20);
     }
 }
