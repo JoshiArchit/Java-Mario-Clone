29f2be8f6e71457acad4d8c8b0d6211a1e59dec2
architJoshi2904 <aj6082@rit.edu>
2023-07-07 11:50:29 +0530

    Animation methods , gravity adjustments

--- src/com/main/mario/entity/Player.java
+++ src/com/main/mario/entity/Player.java
@@ -8,6 +8,11 @@ import com.main.mario.tile.Tile;
 import java.awt.*;
 
 public class Player extends Entity{
+
+    private int frame = 0;
+    private int frameDelay = 0;
+
+    private boolean animate = false;
     public Player( int x, int y, int width, int height, boolean solid, Id id, Handler handler ) {
         // Use entity class constructor
         super( x, y, width, height, solid, id, handler );
@@ -18,7 +23,14 @@ public class Player extends Entity{
 
     @Override
     public void render( Graphics g ) {
-        g.drawImage( Game.player.getBufferedImage(), x, y, width, height, null);
+        //g.drawImage( Game.player.getBufferedImage(), x, y, width, height, null); // For single entity
+        if(facing == 0) {
+            g.drawImage( Game.player[0].getBufferedImage(), x, y, width, height, null);
+        } else if(facing == 1){
+            g.drawImage( Game.player[frame+3].getBufferedImage(), x, y, width, height, null);
+        } else {
+            g.drawImage( Game.player[frame+1].getBufferedImage(), x, y, width, height, null);
+        }
     }
 
     @Override
@@ -28,8 +40,11 @@ public class Player extends Entity{
 
         // Adjust to window bounds
         if (x <= 0) x =0;
-        if (x+width >= 1080) x = 1080 - width;
         if (y+height >= 771) y = 771 - height;
+
+        // Animate
+        if (velX != 0) animate = true;
+        else animate = false;
 
         for( Tile t:handler.tile) {
             if(!t.solid) break;
@@ -39,7 +54,7 @@ public class Player extends Entity{
 //                    y = t.getY() + t.height;
                     if(jumping) {
                         jumping = false;
-                        gravity = 0.0;
+                        gravity = 1.0;
                         falling = true;
                     }
                 }
@@ -68,7 +83,8 @@ public class Player extends Entity{
         }
 
         if(jumping) {
-            gravity -= 0.1;
+            // gravity -= 0.5;
+            gravity -= 0.5;
             setVelY( (int) -gravity );
             if(gravity <= 0.0) {
                 jumping = false;
@@ -77,8 +93,21 @@ public class Player extends Entity{
         }
 
         if(falling) {
-            gravity += 0.1;
+            // gravity += 0.1;
+            gravity += 0.5;
             setVelY( (int) gravity );
         }
+
+        if(animate) {
+            frameDelay ++;
+            if(frameDelay >= 3) {
+                frame ++;
+                if(frame >= 2) {
+                    frame = 0;
+                }
+                frameDelay = 0;
+            }
+        }
+
     }
 }
