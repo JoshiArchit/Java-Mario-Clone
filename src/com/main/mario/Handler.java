d5ad1f0825a60f6938cedaf52399b20250f5c609
architJoshi2904 <aj6082@rit.edu>
2023-07-07 18:44:50 +0530

    createLevel() bug fixes

--- src/com/main/mario/Handler.java
+++ src/com/main/mario/Handler.java
@@ -1,10 +1,12 @@
 package com.main.mario;
 
 import com.main.mario.entity.Entity;
+import com.main.mario.entity.Player;
 import com.main.mario.tile.Tile;
 import com.main.mario.tile.Wall;
 
 import java.awt.*;
+import java.awt.image.BufferedImage;
 import java.util.LinkedList;
 
 public class Handler {
@@ -12,9 +14,9 @@ public class Handler {
     public LinkedList<Entity> entity = new LinkedList<Entity>();
     public LinkedList<Tile> tile = new LinkedList<Tile>();
 
-    public Handler() {
-        createLevel();
-    }
+//    public Handler() {
+//        createLevel(BufferedImage level);
+//    }
 
     public void render( Graphics g) {
         for(Entity en:entity) {
@@ -52,14 +54,36 @@ public class Handler {
         tile.remove( ti );
     }
 
-    public void createLevel() {
-        for(int i = 0; i<Game.WIDTH*Game.SCALE/64 + 1; i++) {
-            addTile( new Wall( i*64, Game.HEIGHT*Game.SCALE-64, 64, 64, true, Id.wall, this ) );
+// Naive level generation
+//    public void createLevel() {
+//        for(int i = 0; i<Game.WIDTH*Game.SCALE/64 + 1; i++) {
+//            addTile( new Wall( i*64, Game.HEIGHT*Game.SCALE-64, 64, 64, true, Id.wall, this ) );
+//
+//            if(i != 0 && i!= 1 && i!= 15 && i != 16 && i!= 17) {
+//                addTile( new Wall( i*64, 340, 64, 64, true, Id.wall, this ) );
+//            }
+//        }
+//    }
 
-            if(i != 0 && i!= 1 && i!= 15 && i != 16 && i!= 17) {
-                addTile( new Wall( i*64, 300, 64, 64, true, Id.wall, this ) );
+    public void createLevel( BufferedImage level) {
+        int width = level.getWidth();
+        int height = level.getHeight();
+
+        for(int y = 0; y < height; y ++) {
+            for(int x = 0; x < width; x ++) {
+                int pixel = level.getRGB( x, y );
+
+                int red = (pixel >> 16) & 0xff;
+                int green = (pixel >> 8) & 0xff;
+                int blue = (pixel) & 0xff;
+
+                if (red==0&&green==0&&blue==0) {        // Black pixel
+                    addTile( new Wall( x*64, y*64, 64,64, true,Id.wall, this ) );
+                }
+                if (red==0 && green == 0 && blue == 255) {    // Blue pixel
+                    addEntity( new Player( x*64, y*64, 64,64, true,Id.player, this ) );
+                }
             }
         }
     }
-
  }
